4a631d9e0cfa6114389d7ca93ff98b7a598fb7d3
bethancordone <43147426+bethancordone@users.noreply.github.com>
2022-11-12 10:53:34 -0500

    Rename boston_energy.sql to content/boston_energy.sql

--- content/boston_energy.sql
+++ content/boston_energy.sql
@@ -83,32 +83,32 @@ ALTER COLUMN [Year Built] INT;
 -- delete rows without property ID
 DELETE FROM Portfolio..Boston_Metrics WHERE [Property Name] is null AND Address is null;
 
--- replace [Site EUI (kBtu/ft²)] with calculation using [Total Site Energy (kBTU)] and Gross Area
+-- replace [Site EUI (kBtu/ftÂ²)] with calculation using [Total Site Energy (kBTU)] and Gross Area
 -- the calculation is the same as the reported value
-SELECT [Total Site Energy (kBTU)], [Site EUI (kBtu/ft²)], [Gross Area (sq ft) _],
+SELECT [Total Site Energy (kBTU)], [Site EUI (kBtu/ftÂ²)], [Gross Area (sq ft) _],
 CASE WHEN [Gross Area (sq ft) _] = 0 THEN null ELSE ROUND([Total Site Energy (kBTU)] / [Gross Area (sq ft) _], 1)
 END AS Calculation
 FROM Portfolio..Boston_Metrics
-WHERE [Site EUI (kBtu/ft²)] is null;
+WHERE [Site EUI (kBtu/ftÂ²)] is null;
 
 -- do the replacement
 UPDATE Portfolio..Boston_Metrics
-SET [Site EUI (kBtu/ft²)] = 
+SET [Site EUI (kBtu/ftÂ²)] = 
 CASE WHEN 
 [Gross Area (sq ft) _] = 0 THEN null ELSE ROUND([Total Site Energy (kBTU)] / [Gross Area (sq ft) _], 1)
 END 
-WHERE [Site EUI (kBtu/ft²)] is null;
+WHERE [Site EUI (kBtu/ftÂ²)] is null;
 
 -- check that it works
 -- in places with values for Area and Total Energy there is a value for Site EUI
-SELECT SUM(CASE WHEN [Total Site Energy (kBTU)] is not null AND [Site EUI (kBtu/ft²)] 
+SELECT SUM(CASE WHEN [Total Site Energy (kBTU)] is not null AND [Site EUI (kBtu/ftÂ²)] 
 is null AND [Gross Area (sq ft) _] is not null 
 AND not [Gross Area (sq ft) _] = 0
 THEN 1 ELSE 0 END) AS Number_Null
 FROM Portfolio..Boston_Metrics;
 
 -- no places to replace Total Site Enery with a calculation
-SELECT SUM(CASE WHEN [Total Site Energy (kBTU)] is null AND [Site EUI (kBtu/ft²)] 
+SELECT SUM(CASE WHEN [Total Site Energy (kBTU)] is null AND [Site EUI (kBtu/ftÂ²)] 
 is not null AND [Gross Area (sq ft) _] is not null 
 AND not [Gross Area (sq ft) _] = 0
 THEN 1 ELSE 0 END) AS Number_Null
@@ -160,11 +160,11 @@ LEFT JOIN Portfolio..Boston_Prop_Info bpi
 -- create a temp table to aid the calculation
 DROP TABLE IF exists #Zip
 CREATE TABLE #Zip
-(Zip nvarchar(5), [Site EUI (kBtu/ft²)] numeric, [Total Site Energy (kBTU)] numeric, [Water Intensity (gal/sf)] numeric, 
+(Zip nvarchar(5), [Site EUI (kBtu/ftÂ²)] numeric, [Total Site Energy (kBTU)] numeric, [Water Intensity (gal/sf)] numeric, 
 [GHG Emissions (MTCO2e)] numeric, [GHG Intensity (kgCO2/sf)] numeric)
 
 INSERT INTO #Zip
-SELECT bprop.[ Zip], bmet.[Site EUI (kBtu/ft²)], bmet.[Total Site Energy (kBTU)], bmet.[Water Intensity (gal/sf)], 
+SELECT bprop.[ Zip], bmet.[Site EUI (kBtu/ftÂ²)], bmet.[Total Site Energy (kBTU)], bmet.[Water Intensity (gal/sf)], 
 bmet.[GHG Emissions (MTCO2e)], bmet.[GHG Intensity (kgCO2/sf)]
 FROM Portfolio..Boston_Prop_Info bprop
 INNER JOIN Portfolio..Boston_Metrics bmet
@@ -175,7 +175,7 @@ WHERE bprop.[ Zip] is not null;
 SELECT * FROM #Zip;
 
 
-SELECT Zip, AVG([Site EUI (kBtu/ft²)]) as [Avg Site EUI], AVG([Total Site Energy (kBTU)]) as [Avg Total Site Energy], AVG([Water Intensity (gal/sf)])
+SELECT Zip, AVG([Site EUI (kBtu/ftÂ²)]) as [Avg Site EUI], AVG([Total Site Energy (kBTU)]) as [Avg Total Site Energy], AVG([Water Intensity (gal/sf)])
 as [Avg Water Intensity], 
 AVG([GHG Emissions (MTCO2e)]) as [Avg GHG Emissions], AVG([GHG Intensity (kgCO2/sf)]) as [Avg GHG Intensity]
 FROM #Zip
@@ -185,7 +185,7 @@ ORDER BY [Avg Site EUI] desc;
 -- look at the median differences
 -- for visualizations might want to use median as there are outliers that skew the average
 
-SELECT DISTINCT Zip, PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY [Site EUI (kBtu/ft²)]) OVER (PARTITION BY Zip) as [Median Site EUI], 
+SELECT DISTINCT Zip, PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY [Site EUI (kBtu/ftÂ²)]) OVER (PARTITION BY Zip) as [Median Site EUI], 
 PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY [Total Site Energy (kBTU)]) OVER (PARTITION BY Zip) as [Median Total Site Energy],
 PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY [Water Intensity (gal/sf)]) OVER (PARTITION BY Zip) as [Median Water Intensity],
 PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY [GHG Emissions (MTCO2e)]) OVER (PARTITION BY Zip) as [Median GHG Emissions],
@@ -197,7 +197,7 @@ ORDER BY [Median Site EUI] desc;
 -- there appears to be some difference across years
 -- could be due to reporting issues or actual trend (2017 has a lot of energy use)
 
-SELECT Year, AVG([Site EUI (kBtu/ft²)]) as [Avg Site EUI], AVG([Total Site Energy (kBTU)]) as [Avg Total Site Energy], AVG([Water Intensity (gal/sf)])
+SELECT Year, AVG([Site EUI (kBtu/ftÂ²)]) as [Avg Site EUI], AVG([Total Site Energy (kBTU)]) as [Avg Total Site Energy], AVG([Water Intensity (gal/sf)])
 as [Avg Water Intensity], 
 AVG([GHG Emissions (MTCO2e)]) as [Avg GHG Emissions], AVG([GHG Intensity (kgCO2/sf)]) as [Avg GHG Intensity]
 FROM Portfolio..Boston_Metrics
@@ -205,7 +205,7 @@ GROUP BY Year
 ORDER BY [Avg Site EUI] DESC;
 
 -- the medians are much closer to each other than the averages
-SELECT DISTINCT Year, PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY [Site EUI (kBtu/ft²)]) OVER (PARTITION BY Year) as [Median Site EUI], 
+SELECT DISTINCT Year, PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY [Site EUI (kBtu/ftÂ²)]) OVER (PARTITION BY Year) as [Median Site EUI], 
 PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY [Total Site Energy (kBTU)]) OVER (PARTITION BY Year) as [Median Total Site Energy],
 PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY [Water Intensity (gal/sf)]) OVER (PARTITION BY Year) as [Median Water Intensity],
 PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY [GHG Emissions (MTCO2e)]) OVER (PARTITION BY Year) as [Median GHG Emissions],
@@ -219,11 +219,11 @@ SELECT Street FROM Portfolio..Boston_Prop_Info;
 -- try the streets
 DROP TABLE IF exists #Street
 CREATE TABLE #Street
-(Street nvarchar(250), [Site EUI (kBtu/ft²)] numeric, [Total Site Energy (kBTU)] numeric, [Water Intensity (gal/sf)] numeric, 
+(Street nvarchar(250), [Site EUI (kBtu/ftÂ²)] numeric, [Total Site Energy (kBTU)] numeric, [Water Intensity (gal/sf)] numeric, 
 [GHG Emissions (MTCO2e)] numeric, [GHG Intensity (kgCO2/sf)] numeric)
 
 INSERT INTO #Street
-SELECT bprop.Street, bmet.[Site EUI (kBtu/ft²)], bmet.[Total Site Energy (kBTU)], bmet.[Water Intensity (gal/sf)], 
+SELECT bprop.Street, bmet.[Site EUI (kBtu/ftÂ²)], bmet.[Total Site Energy (kBTU)], bmet.[Water Intensity (gal/sf)], 
 bmet.[GHG Emissions (MTCO2e)], bmet.[GHG Intensity (kgCO2/sf)]
 FROM Portfolio..Boston_Prop_Info bprop
 INNER JOIN Portfolio..Boston_Metrics bmet
@@ -233,55 +233,55 @@ WHERE bprop.Street is not null;
 
 SELECT * FROM #Street;
 
-SELECT Street, AVG([Site EUI (kBtu/ft²)]) as [Avg Site EUI], AVG([Total Site Energy (kBTU)]) as [Avg Total Site Energy], AVG([Water Intensity (gal/sf)])
+SELECT Street, AVG([Site EUI (kBtu/ftÂ²)]) as [Avg Site EUI], AVG([Total Site Energy (kBTU)]) as [Avg Total Site Energy], AVG([Water Intensity (gal/sf)])
 as [Avg Water Intensity], 
 AVG([GHG Emissions (MTCO2e)]) as [Avg GHG Emissions], AVG([GHG Intensity (kgCO2/sf)]) as [Avg GHG Intensity]
 FROM #Street
-WHERE [Site EUI (kBtu/ft²)] is not null
+WHERE [Site EUI (kBtu/ftÂ²)] is not null
 GROUP BY Street
 ORDER BY [Avg Site EUI] desc;
 
 -- look at the min/max values
 
-SELECT Street, AVG([Site EUI (kBtu/ft²)]) as [Avg Site EUI], AVG([Total Site Energy (kBTU)]) as [Avg Total Site Energy], AVG([Water Intensity (gal/sf)])
+SELECT Street, AVG([Site EUI (kBtu/ftÂ²)]) as [Avg Site EUI], AVG([Total Site Energy (kBTU)]) as [Avg Total Site Energy], AVG([Water Intensity (gal/sf)])
 as [Avg Water Intensity], 
 AVG([GHG Emissions (MTCO2e)]) as [Avg GHG Emissions], AVG([GHG Intensity (kgCO2/sf)]) as [Avg GHG Intensity]
 FROM #Street
-WHERE [Site EUI (kBtu/ft²)] is not null
+WHERE [Site EUI (kBtu/ftÂ²)] is not null
 GROUP BY Street
 ORDER BY [Avg Site EUI] desc;
 
 
 -- look at the ranges
-SELECT Street, MAX([Site EUI (kBtu/ft²)]) - MIN([Site EUI (kBtu/ft²)]) as [Range Site EUI], 
+SELECT Street, MAX([Site EUI (kBtu/ftÂ²)]) - MIN([Site EUI (kBtu/ftÂ²)]) as [Range Site EUI], 
 MAX([Total Site Energy (kBTU)]) - MIN([Total Site Energy (kBTU)]) as [Range Total Site Energy],
 MAX([Water Intensity (gal/sf)]) - MIN([Water Intensity (gal/sf)]) as [Range Water Intensity],
 MAX([GHG Emissions (MTCO2e)]) - MIN([GHG Emissions (MTCO2e)]) as [Range GHG Emissions],
 MAX([GHG Intensity (kgCO2/sf)]) - MIN([GHG Intensity (kgCO2/sf)]) as [Range GHG Intensity]
 FROM #Street
-WHERE [Site EUI (kBtu/ft²)] is not null
+WHERE [Site EUI (kBtu/ftÂ²)] is not null
 GROUP BY Street
 ORDER BY [Range Site EUI] desc;
 
 
 -- look at SENIOR, K 12 School, Single Family Home, PRE SCHOOL, RACE TRACK, STADIUM, HOSPITAL, LABORATORY, OFFICE, DATA CENTER, ect.
 -- CTE makes calculation easier
-WITH GroupPropertyType ([Property Type], [Site EUI (kBtu/ft²)], [Total Site Energy (kBTU)], [Water Intensity (gal/sf)], 
+WITH GroupPropertyType ([Property Type], [Site EUI (kBtu/ftÂ²)], [Total Site Energy (kBTU)], [Water Intensity (gal/sf)], 
 [GHG Emissions (MTCO2e)], [GHG Intensity (kgCO2/sf)])
 AS
 (
-SELECT bprop.[Property Type], bmet.[Site EUI (kBtu/ft²)], bmet.[Total Site Energy (kBTU)], bmet.[Water Intensity (gal/sf)], 
+SELECT bprop.[Property Type], bmet.[Site EUI (kBtu/ftÂ²)], bmet.[Total Site Energy (kBTU)], bmet.[Water Intensity (gal/sf)], 
 bmet.[GHG Emissions (MTCO2e)], bmet.[GHG Intensity (kgCO2/sf)]
 FROM Portfolio..Boston_Prop_Info bprop
 INNER JOIN Portfolio..Boston_Metrics bmet
 	ON bprop.[Property Name] = bmet.[Property Name]
 	AND bprop.[Address] = bmet.[Property Name]
 )
-SELECT [Property Type], AVG([Site EUI (kBtu/ft²)]) as [Avg Site EUI], AVG([Total Site Energy (kBTU)]) as [Avg Total Site Energy], AVG([Water Intensity (gal/sf)])
+SELECT [Property Type], AVG([Site EUI (kBtu/ftÂ²)]) as [Avg Site EUI], AVG([Total Site Energy (kBTU)]) as [Avg Total Site Energy], AVG([Water Intensity (gal/sf)])
 as [Avg Water Intensity], 
 AVG([GHG Emissions (MTCO2e)]) as [Avg GHG Emissions], AVG([GHG Intensity (kgCO2/sf)]) as [Avg GHG Intensity]
 FROM GroupPropertyType
-WHERE [Site EUI (kBtu/ft²)] is not null
+WHERE [Site EUI (kBtu/ftÂ²)] is not null
 GROUP BY [Property Type]
 ORDER BY [Avg Site EUI] desc;
 
@@ -290,22 +290,22 @@ ORDER BY [Avg Site EUI] desc;
 SELECT MAX([Year Built]) as [Max Year Built], MIN([Year Built]) as [Min Year Built] FROM Portfolio..Boston_Prop_Info;
 
 -- Group the years in to 25 year segments and look at metrics across each section
-WITH GroupYearBuilt ([Year Built], [Site EUI (kBtu/ft²)], [Total Site Energy (kBTU)], [Water Intensity (gal/sf)], 
+WITH GroupYearBuilt ([Year Built], [Site EUI (kBtu/ftÂ²)], [Total Site Energy (kBTU)], [Water Intensity (gal/sf)], 
 [GHG Emissions (MTCO2e)], [GHG Intensity (kgCO2/sf)])
 AS
 (
-SELECT bprop.[Year Built], bmet.[Site EUI (kBtu/ft²)], bmet.[Total Site Energy (kBTU)], bmet.[Water Intensity (gal/sf)], 
+SELECT bprop.[Year Built], bmet.[Site EUI (kBtu/ftÂ²)], bmet.[Total Site Energy (kBTU)], bmet.[Water Intensity (gal/sf)], 
 bmet.[GHG Emissions (MTCO2e)], bmet.[GHG Intensity (kgCO2/sf)]
 FROM Portfolio..Boston_Prop_Info bprop
 INNER JOIN Portfolio..Boston_Metrics bmet
 	ON bprop.[Property Name] = bmet.[Property Name]
 	AND bprop.[Address] = bmet.[Property Name]
 )
-SELECT t.Range as [Year Range], AVG([Site EUI (kBtu/ft²)]) as [Avg Site EUI], AVG([Total Site Energy (kBTU)]) as [Avg Total Site Energy],
+SELECT t.Range as [Year Range], AVG([Site EUI (kBtu/ftÂ²)]) as [Avg Site EUI], AVG([Total Site Energy (kBTU)]) as [Avg Total Site Energy],
 AVG([Water Intensity (gal/sf)]) as [Avg Water Intensity], 
 AVG([GHG Emissions (MTCO2e)]) as [Avg GHG Emissions], AVG([GHG Intensity (kgCO2/sf)]) as [Avg GHG Intensity]
 FROM (
-      SELECT [Year Built], [Site EUI (kBtu/ft²)], [Total Site Energy (kBTU)], [Water Intensity (gal/sf)], 
+      SELECT [Year Built], [Site EUI (kBtu/ftÂ²)], [Total Site Energy (kBTU)], [Water Intensity (gal/sf)], 
 		[GHG Emissions (MTCO2e)], [GHG Intensity (kgCO2/sf)],
 		 CASE WHEN [Year Built] BETWEEN 1800 AND 1824 THEN '1800-1824'
          WHEN [Year Built] BETWEEN 1825 AND 1849 THEN '1825-1849'
@@ -349,7 +349,7 @@ CASE WHEN bpi.[Year Built] BETWEEN 1800 AND 1824 THEN '1800-1824'
 		 WHEN bpi.[Year Built] BETWEEN 1975 AND 1999 THEN '1977-1999'
          ELSE '2000-2020' END as [Year Built Range],
 bpi.[Property Type], bpi.[Property Uses],
-bmet.Year, bmet.[Gross Area (sq ft) _], bmet.[Site EUI (kBtu/ft²)], bmet.[ENERGY STAR Score],
+bmet.Year, bmet.[Gross Area (sq ft) _], bmet.[Site EUI (kBtu/ftÂ²)], bmet.[ENERGY STAR Score],
 bmet.[Total Site Energy (kBTU)], bmet.[Water Intensity (gal/sf)],
 bmet.[Water Intensity (gal/sf)] * bmet.[Gross Area (sq ft) _] as [Total Water (gal)], 
 bmet.[GHG Emissions (MTCO2e)], bmet.[GHG Intensity (kgCO2/sf)],
@@ -361,7 +361,7 @@ INNER JOIN Portfolio..Boston_Metrics bmet
 INNER JOIN Portfolio..Boston_Prop_Info bpi
 	ON bid.[Property Name] = bpi.[Property Name]
 	AND bid.[Address] = bpi.[Address]
-WHERE bmet.[Site EUI (kBtu/ft²)] is not null OR 
+WHERE bmet.[Site EUI (kBtu/ftÂ²)] is not null OR 
 	bmet.[ENERGY STAR Score] is not null OR
 	bmet.[Total Site Energy (kBTU)] is not null OR
 	bmet.[Water Intensity (gal/sf)] is not null OR
@@ -373,6 +373,6 @@ WHERE bmet.[Site EUI (kBtu/ft²)] is not null OR
 -- notes for visualizations
 -- the WANG Theature had a large amount of electricity use in 2017 this will impact results if using average
 
-SELECT * FROM Boston_Metrics_IDs WHERE [Site EUI (kBtu/ft²)] is not null ORDER BY [Site EUI (kBtu/ft²)] DESC;
+SELECT * FROM Boston_Metrics_IDs WHERE [Site EUI (kBtu/ftÂ²)] is not null ORDER BY [Site EUI (kBtu/ftÂ²)] DESC;
 
 SELECT * FROM Portfolio..Boston_Prop_Info WHERE Street like 'Tremont St';
